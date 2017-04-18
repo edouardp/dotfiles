678aa8d69ac4c8e7023bff4c79eb8a3f4a4b8a8c
Edouard Poor <github@edouard.info>
2017-04-18 22:12:25 +1200

    Added npm.commands, updated bash prompt

--- prompt.bash
+++ prompt.bash
@@ -4,7 +4,7 @@ function parse_git_branch() {
     if [ ! "${BRANCH}" == "" ]
     then
         STAT=`parse_git_dirty`
-        echo " [ ${BRANCH}${STAT}]"
+        echo " ${BRANCH}${STAT}"
     else
         echo ""
     fi
@@ -20,23 +20,14 @@ function parse_git_dirty {
     renamed=`echo -n "${status}" 2> /dev/null | grep "renamed:" &> /dev/null; echo "$?"`
     deleted=`echo -n "${status}" 2> /dev/null | grep "deleted:" &> /dev/null; echo "$?"`
     bits=''
-    if [ "${renamed}" == "0" ]; then
-        bits=">${bits}"
-    fi
     if [ "${ahead}" == "0" ]; then
-        bits="*${bits}"
+        bits=" ${bits}"
     fi
-    if [ "${newfile}" == "0" ]; then
-        bits="+${bits}"
+    if [ "${renamed}" == "0" ] || [ "${newfile}" == "0" ] || [ "${deleted}" == "0" ] || [ "${dirty}" == "0" ]; then
+        bits=" ${bits}"
     fi
     if [ "${untracked}" == "0" ]; then
-        bits="?${bits}"
-    fi
-    if [ "${deleted}" == "0" ]; then
-        bits="x${bits}"
-    fi
-    if [ "${dirty}" == "0" ]; then
-        bits="!${bits}"
+        bits=" ${bits}"
     fi
     if [ ! "${bits}" == "" ]; then
         echo " ${bits}"
