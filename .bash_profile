cde3dec1777eaef0ae8da0b70cfec0612ff82e95
EdouardMYOB <35671932+EdouardMYOB@users.noreply.github.com>
2018-05-03 09:30:49 +1200

    Adding fancy prompt to bashrc, adding asci prompt (for cathode et al)

--- .bash_profile
+++ .bash_profile
@@ -1,7 +1,26 @@
 eval $(/usr/libexec/path_helper -s)
 
+export PATH=/usr/local/opt/pyenv/libexec:$PATH
+
 # Load bash prompt
 #
-. prompt.bash
+. ~/.dotfiles/prompt.bash
+
+export CLICOLOR=1
+export LSCOLORS=GxFxCxDxBxegedabagaced
 
 
+# Hack for pyenv axctivate
+eval "$(pyenv init -)"
+
+# Bash Conmpletion
+[ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion
+
+# Aliases
+alias ll="ls -lv"
+alias la='ll -A'           #  Show hidden files.
+
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
---
+++ prompt-ascii.bash
@@ -0,0 +1,80 @@
+# get current branch in git repo
+function parse_git_branch() {
+    BRANCH=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
+    if [ ! "${BRANCH}" == "" ]
+    then
+        STAT=`parse_git_dirty`
+        echo " ${BRANCH}${STAT}"
+    else
+        echo ""
+    fi
+}
+
+# get current status of git repo
+function parse_git_dirty {
+    status=`git status 2>&1 | tee`
+    dirty=`echo -n "${status}" 2> /dev/null | grep "modified:" &> /dev/null; echo "$?"`
+    untracked=`echo -n "${status}" 2> /dev/null | grep "Untracked files" &> /dev/null; echo "$?"`
+    ahead=`echo -n "${status}" 2> /dev/null | grep "Your branch is ahead of" &> /dev/null; echo "$?"`
+    behind=`echo -n "${status}" 2> /dev/null | grep "Your branch is behind" &> /dev/null; echo "$?"`
+    diverged=`echo -n "${status}" 2> /dev/null | grep "have diverged" &> /dev/null; echo "$?"`
+    newfile=`echo -n "${status}" 2> /dev/null | grep "new file:" &> /dev/null; echo "$?"`
+    renamed=`echo -n "${status}" 2> /dev/null | grep "renamed:" &> /dev/null; echo "$?"`
+    deleted=`echo -n "${status}" 2> /dev/null | grep "deleted:" &> /dev/null; echo "$?"`
+    bits=''
+    if [ "${ahead}" == "0" ]; then
+        bits="^${bits}"
+    fi
+    if [ "${behind}" == "0" ]; then
+        bits="v${bits}"
+    fi
+    if [ "${diverged}" == "0" ]; then
+        bits="*${bits}"
+    fi
+    if [ "${renamed}" == "0" ] || [ "${newfile}" == "0" ] || [ "${deleted}" == "0" ] || [ "${dirty}" == "0" ]; then
+        bits="+${bits}"
+    fi
+    if [ "${untracked}" == "0" ]; then
+        bits="?${bits}"
+    fi
+    if [ ! "${bits}" == "" ]; then
+        echo " ${bits}"
+    else
+        echo ""
+    fi
+}
+
+function initial_colour() {
+    RETVAL=$?
+    if [[ $RETVAL -ne 0 ]]; then
+        echo -e "\033[38;5;196m";
+    else
+        echo -e "\033[38;5;15m";
+    fi
+    (exit $RETVAL)  # Set $? via hideous hack
+}
+
+
+function last_status() {
+    RETVAL=$? 
+    if [[ $RETVAL -ne 0 ]]; then
+        echo "!!! ";
+    else
+        if [ -f /etc/os-release ]
+        then
+            if grep -iq ubuntu /etc/os-release; then echo ""; fi
+            if grep -iq raspbian /etc/os-release; then echo ""; fi
+        else
+            echo ""
+        fi
+        #echo $OSCHAR
+        #echo "";
+        #echo "";
+        #echo "";
+    fi
+}
+
+
+
+export PS1="\[\$(initial_colour)\]\`last_status\`\[\e[38;5;99m\]\h \[\e[38;5;45m\]\w\[\e[38;5;226m\]\`parse_git_branch\` \[\e[38;5;15m\]> "
+
